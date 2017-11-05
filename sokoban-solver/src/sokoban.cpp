0d17ea212998f3bc3faa12c560e642493fa2a944
jlrandulfe <jlrandulfe@gmail.com>
2017-11-05 20:26:41 +0100

    Add a comment block to each method in the puzzle class

--- sokoban-solver/src/sokoban.cpp
+++ sokoban-solver/src/sokoban.cpp
@@ -18,18 +18,36 @@ sokoban::SokobanPuzzle::SokobanPuzzle(int diamonds, int width, int height) {
     this->current_state.resize(this->num_of_diamonds+1, vector <int> (2));
 }
 
+/**
+    Set an element of the walkable_squares matrix to a given value.
+
+    A value of '1' or true represents a walkable square, while a '0'
+    or false represents a non-walkable square.
+*/
 void sokoban::SokobanPuzzle::set_walkable_square(int x_coord, int y_coord,
                                                  bool value) {
     this->walkable_squares[x_coord][y_coord] = value;
     return;
 }
 
+/**
+    Set an element of the goal_squares matrix to a given value.
+
+    A value of '1' or true represents a goal square, while a '0'
+    or false represents a non-goal square.
+*/
 void sokoban::SokobanPuzzle::set_goal_square(int x_coord, int y_coord,
                                              bool value) {
     this->goal_squares[x_coord][y_coord] = value;
     return;
 }
 
+/**
+    Update the position of the man/player in the puzzle.
+
+    The position is represented by the 2-D cartesian coordinates, and
+    is stored in the first sub-vector of the current_state vector.
+*/
 void sokoban::SokobanPuzzle::update_player_position(int x_coord, int y_coord) {
     // Store the Cartesian coordinates, in the format (X, Y)
     this->current_state[0][0] = x_coord;
@@ -37,6 +55,13 @@ void sokoban::SokobanPuzzle::update_player_position(int x_coord, int y_coord) {
     return;
 }
 
+/**
+    Update the position of a box/diamond in the puzzle.
+
+    The position is represented by the 2-D cartesian coordinates, and
+    is stored in one of the sub-vectors of the current_state vector,
+    excepting the first one, that corresponds to the man/player.
+*/
 void sokoban::SokobanPuzzle::update_box_position(int box_index, int x_coord,
                                                  int y_coord) {
     // Store the Cartesian coordinates, in the format (X, Y)
@@ -45,6 +70,12 @@ void sokoban::SokobanPuzzle::update_box_position(int box_index, int x_coord,
     return;
 }
 
+/**
+    Analyze if the current state of the puzzle is the desired goal.
+
+    For that purpose, it is checked if every box/diamond coordinates
+    correspond to a puzzle goal position.
+*/
 bool sokoban::SokobanPuzzle::goal_reached() {
     // Returned boolean, indicating if the goal has been reached.
     bool success = true;
@@ -61,10 +92,16 @@ bool sokoban::SokobanPuzzle::goal_reached() {
     return success;
 }
 
+/**
+    Return the number of boxes/diamonds in the puzzle.
+*/
 int sokoban::SokobanPuzzle::get_diamonds() {
     return this->num_of_diamonds;
 }
 
+/**
+    Return true if the selected square is walkable.
+*/
 int sokoban::SokobanPuzzle::get_walkable_square(int x_coord, int y_coord) {
     int value  = this->walkable_squares[x_coord][y_coord];
     return value;
