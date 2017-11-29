8ae1abc3e9f2b1077e623f99ece172d03524a8cc
noeoeh <groth@live.dk>
2017-11-29 20:08:48 +0100

    Able to drive backwards

--- src/DeliverCan.h
+++ src/DeliverCan.h
@@ -1,53 +1,105 @@
 motorStruct DeliverCan(sensorBoolStruct inputSensors, int state, char  speed, char comp){
     motorStruct returnMovement;
+	
+	bool onTrack = 	  !inputSensors.middle && !inputSensors.left; //On track
+	bool compRight =  !inputSensors.middle &&  inputSensors.left; //Compensate from right
+	bool compLeft =    inputSensors.middle && !inputSensors.left; //Compensate from left
+	bool placed = 	   inputSensors.right;						  //Can placed
+	bool cross = 	   inputSensors.middle &&  inputSensors.left; //Cross
+            
 
     if (state == 0){//On cross when start
-       if (!inputSensors.middle      //Drive until out of cross
-       && !inputSensors.left){
+       if (onTrack){      //Drive until out of cross
           returnMovement.state = 10;
        }
+		else{
+			returnMovement.state = 0;
+		}
+	
        returnMovement.speed =  speed;
        returnMovement.turn = 0;
     }
     else if (state == 10){ //On track
-       if (inputSensors.middle           //Compensate from left
-            && !inputSensors.left)
-               returnMovement.state = 20;
-       else if (!inputSensors.middle     //Compensate from right
-            && inputSensors.left)
-               returnMovement.state = 30;
-       else if (inputSensors.right)      //Stop to deliver
-               returnMovement.state = 99;
+       if (compLeft)          //Compensate from left
+            returnMovement.state = 20;
+       else if (compRight)
+            returnMovement.state = 30;
+       else if (placed)      //Stop to deliver
+            returnMovement.state = 60;
+		else{
+			returnMovement.state = 10;
+		}
                
        returnMovement.speed =  speed;
        returnMovement.turn = 0;
     }
     else if (state == 20){ //Compensate from left
-       if (!inputSensors.middle          //On track
-            && !inputSensors.left)
+       if (onTrack)
                returnMovement.state = 10;
-       else if (!inputSensors.middle     //Compensate from right
-            && inputSensors.left)
+       else if (compRight)
                returnMovement.state = 30;
-       else if (inputSensors.right)      //Stop to deliver
-               returnMovement.state = 99;
+       else if (placed)      //Stop to deliver
+               returnMovement.state = 60;
+		else{
+		returnMovement.state = 20;
+		}
                
        returnMovement.speed =  speed;
        returnMovement.turn = -comp;
     }
     else if (state == 30){ //Compensate from right
-       if (!inputSensors.middle          //On track
-            && !inputSensors.left)
+       if (onTrack)
                returnMovement.state = 10;
-       else if (inputSensors.middle      //Compensate from left
-            && !inputSensors.left)
+       else if (compLeft)
                returnMovement.state = 20;
-       else if (inputSensors.right)      //Stop to deliver
-               returnMovement.state = 99;
+       else if (placed)      //Stop to deliver
+               returnMovement.state = 60;
+		else{
+		returnMovement.state = 30;
+		}
                
        returnMovement.speed =  speed;
        returnMovement.turn = comp;
     }
+	
+	// backwards
+	
+    else if (state == 60){ //On track
+       if (cross)
+               returnMovement.state = 99;
+		else{
+			returnMovement.state = 60;
+		}
+               
+       returnMovement.speed = speed;
+       returnMovement.turn = 0;
+    }
+    else if (state == 99){ //Stop at cross
+    
+       returnMovement.speed = 0;
+       returnMovement.turn = 0;
+	   returnMovement.state = 99;
+    }
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
     else if (state == 99){ //Stop at cross
     
        returnMovement.speed = 0;
